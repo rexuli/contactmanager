316daa1647cc93487fc8ecb2c1a2c4206237e553
rexuli <li311331543@hotmail.com>
2019-01-03 23:53:33 +0800

    Updates

---
+++ precache-manifest.ea50cc1f6601c583d27bb157b7469a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2fbb42746d8952057c5",
+    "url": "/rexuli/contactmanager/static/js/runtime~main.b2fbb427.js"
+  },
+  {
+    "revision": "62599ae7fe63a6eaa350",
+    "url": "/rexuli/contactmanager/static/js/main.62599ae7.chunk.js"
+  },
+  {
+    "revision": "f96a6c0ac197713adaf6",
+    "url": "/rexuli/contactmanager/static/js/1.f96a6c0a.chunk.js"
+  },
+  {
+    "revision": "62599ae7fe63a6eaa350",
+    "url": "/rexuli/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "f96a6c0ac197713adaf6",
+    "url": "/rexuli/contactmanager/static/css/1.d0a9569f.chunk.css"
+  },
+  {
+    "revision": "d5e5e7af55e11d91d69ff525ee5580b5",
+    "url": "/rexuli/contactmanager/index.html"
+  }
+];
